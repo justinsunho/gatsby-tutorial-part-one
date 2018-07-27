c2a3ee44f21831f469fc5b619cd37beb32092b55
Justin Sun-ho Kim <justin.kim@liferay.com>
2018-07-27 12:43:45 -0700

    Tutorial Step 1

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -3,12 +3,7 @@ import Link from "gatsby-link"
 
 export default () =>
 	<div style= {{ color: `blue` }}>
-		<h1>Hello world!</h1>
-		<p>asdf</p>
 		<div>
-			<Link to="/page-2/">HERES A LINK</Link>
-		</div>
-		<div>
-			<Link to="/counter/">Counter</Link>
+			<Link to="/">HOME</Link>
 		</div>
 	</div>
